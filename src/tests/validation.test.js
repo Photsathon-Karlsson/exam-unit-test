44506faea9e5d111180e93f9b875c30fbad2c8fa
KoiOriginal <photsathon.sangsukwaw@gmail.com>
2025-05-28 09:34:44 +0200

    Add validation.js

--- src/tests/validation.test.js
+++ src/tests/validation.test.js
@@ -11,18 +11,55 @@ const exampleCartObject = {
 	amount: 1,
 	item: exampleProduct
 }
+/*
+// For invalid cases.
+const exampleInvalidProduct = {
+  id: 1001,
+  price: 500,
+};
 
+const exampleInvalidCartObject = {
+  amount: 1,
+  item: exampleProduct,
+};
+*/
 // Group tests using "describe"
 describe('Validation', () => {
 
-	// Använd en "test" eller "it" (de är synonymer) för varje testfall
+	// True cases
+	// Cases where the functions work correctly and return the expected result.
+	describe("True cases", () => {
+    // isProduct()
+    it("isProduct() should return true for a valid product", () => {
+      const result = isProduct(exampleProduct); // The result should be true.
+      // console.log("result of isProduct() is ", result);
+
+      expect(result).toBe(true); // Check that result is true.
+    });
+
+	// isCartItem()
+    it("isCartItem() should return true for a valid cart item", () => {
+      const result = isCartItem(exampleCartObject); 
+      // console.log("result of isCartItem() is ", result);
+
+      expect(result).toBe(true); 
+    });
+  });
+
+	// False cases
+
+
+	//Throw error cases
+
+})
+
+// Använd en "test" eller "it" (de är synonymer) för varje testfall
 	/* Exempel på syntax:
 	test('beskriv testfallet', () => {
 		// här skriver du testkoden
 		// avsluta alltid med "expect"
 	})
 	*/
-
 
 	// ---------------------------------------------
 	// Följande testfall ska du implementera. Det är tillåtet att använda Joi. Gör i så fall ett schema för varje sorts objekt du vill kunna validera. Du får även ändra texten och du t.ex. vill skriva på svenska i stället för engelska.
@@ -33,4 +70,3 @@ describe('Validation', () => {
 
 	// 3. it returns true for a valid product
 	// 4. it returns false for invalid cart objects
-})
--- src/validation.js
+++ src/validation.js
@@ -4,10 +4,64 @@
 // 3. write code in this file so that the test case becomes GREEN
 // 4. refactor as neccessary before you move on to the next
 // 5. repeat
+//-------------------------------------------------------------------//
 
-function isCartItem(maybeCartItem) {}
+// All 3 properties must not be undefined, & id and amount must be numbers, & item must be an object.
+// Check item is a product. If it's a product then return true.
+// If it's not a product, then return false.
+function isCartItem(maybeCartItem) {
+   if (maybeCartItem.id && maybeCartItem.amount && maybeCartItem.item) {
+    // Check if it has all properties.
+    if (
+      typeof maybeCartItem.id === "number" &&
+      typeof maybeCartItem.amount === "number" &&
+      typeof maybeCartItem.item === "object"
+    ) {
+      // Check if the properties have the correct types.
+      if (
+        maybeCartItem.item.id &&
+        maybeCartItem.item.name &&
+        maybeCartItem.item.price &&
+        typeof maybeCartItem.item.id === "number" &&
+        typeof maybeCartItem.item.name === "string" &&
+        typeof maybeCartItem.item.price === "number"
+      ) {
+        // Check if item has all properties & correct types.
+        return true;
+      } else {
+        // If item is invalid (missing properties or incorrect types).
+        return false;
+      }
+    } else {
+      // If the properties have incorrect types.
+      return false;
+    }
+    } else {
+        // If properties are missing.
+        return false;
+    } 
+}
 
-function isProduct(maybeProduct) {}
-
+// All 3 properties must not be undefined, id and price = numbers, name = string, in order to return true.
+// If not, return false.
+function isProduct(maybeProduct) {
+    if (maybeProduct.id && maybeProduct.name && maybeProduct.price) {
+    // Check if it has all properties.
+    if (
+      typeof maybeProduct.id === "number" &&
+      typeof maybeProduct.name === "string" &&
+      typeof maybeProduct.price === "number"
+    ) {
+      // Check if item has all properties & correct types.
+      return true;
+    } else {
+      // If the properties have incorrect types.
+      return false;
+    }
+  } else {
+    // If properties are missing.
+    return false;
+  }
+}
 
 export { isCartItem, isProduct }
